c62aaf4e151f5c6c47359d103c7cabd78ad3bfc4
Naulian <cinkhangin@codigo.co>
2024-01-07 11:48:29 +0630

    initial setup

--- modify/src/main/java/com/naulian/modify/Typography.kt
+++ modify/src/main/java/com/naulian/modify/Typography.kt
@@ -1,2 +1,7 @@
 package com.naulian.modify
 
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+
+fun TextStyle.normal() = copy(fontWeight = FontWeight.Normal)
+fun TextStyle.bold() = copy(fontWeight = FontWeight.Bold)
