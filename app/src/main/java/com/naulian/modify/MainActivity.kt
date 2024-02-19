0d9e7d1c1910ff40877d17c8968d7aea6ebbd814
Naulian <150779672+naulian-codigo@users.noreply.github.com>
2024-02-19 15:24:40 +0630

    release 0.0.1-alpha02

--- app/src/main/java/com/naulian/modify/MainActivity.kt
+++ app/src/main/java/com/naulian/modify/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,7 +22,6 @@ class MainActivity : ComponentActivity() {
                     color = themeColors.surface
                 ) {
                     Greeting("Android")
-                    MaterialTheme.typography.bodyLarge
                 }
             }
         }
--- app/src/main/java/com/naulian/modify/ui/theme/Theme.kt
+++ app/src/main/java/com/naulian/modify/ui/theme/Theme.kt
@@ -64,7 +64,7 @@ fun ModifyTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = MyTypography,
         content = content
     )
 }
