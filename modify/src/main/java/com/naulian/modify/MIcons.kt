d9b7466d19c48e9f0722d0831dc95ce77ee0d1e3
Naulian <cinkhangin@gmail.com>
2024-12-24 22:30:07 +0800

    release 0.1.0-beta02

---
+++ modify/src/main/java/com/naulian/modify/MIcons.kt
@@ -0,0 +1,19 @@
+package com.naulian.modify
+
+//MIcons icons are from hugeicons.com
+object MIcons {
+    val Copy = R.drawable.ic_copy
+    val Back = R.drawable.ic_back
+    val Cancel = R.drawable.ic_cancel
+    val Delete = R.drawable.ic_delete
+    val Settings = R.drawable.ic_settings
+    val Search = R.drawable.ic_search
+    val Notification = R.drawable.ic_notification
+    val Image = R.drawable.ic_image
+    val Add = R.drawable.ic_add
+
+    val edit = R.drawable.ic_pencil2
+    val edit1 = R.drawable.ic_edit1
+    val edit2 = R.drawable.ic_edit2
+    val edit3 = R.drawable.ic_pencil1
+}
--- modify/src/main/java/com/naulian/modify/Modicons.kt
+++
@@ -1,8 +0,0 @@
-package com.naulian.modify
-
-//modicons icons are from hugeicons.com
-object MIcons {
-    val Copy = R.drawable.ic_copy
-    val Back = R.drawable.ic_back
-    val Cancel = R.drawable.ic_cancel
-}
